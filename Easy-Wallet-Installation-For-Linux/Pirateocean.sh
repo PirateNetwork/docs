6c8e86c29c0bf81f9b1a4a05a9247884cc91eedf
Kaks <65034388+kaks25@users.noreply.github.com>
2020-08-29 00:37:48 +0200

    Pirateocean

--- Easy-Wallet-Installation-For-Linux/Pirateocean.sh
+++ Easy-Wallet-Installation-For-Linux/Pirateocean.sh
@@ -13,14 +13,14 @@ echo Installing Pirateocean package
 sudo apt-get install libminiupnpc17
 
 echo Creating /.komodo/PIRATE Folder
-mkdir -p /home/$USER/.komodo/PIRATE && chmod 644 /home/$USER/.komodo/PIRATE
+mkdir -p /home/$USER/.komodo/PIRATE && chmod 755 /home/$USER/.komodo/PIRATE
 echo Installing DOWNLOAD BOOTSRAPS
 wget -N --no-check-certificate --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://eu.bootstrap.dexstats.info/PIRATE-bootstrap.tar.gz -P /home/$USER/.komodo/PIRATE
 tar -xzvf /home/$USER/.komodo/PIRATE/PIRATE-bootstrap.tar.gz -C /home/$USER/.komodo/PIRATE
 rm ~/.komodo/PIRATE/PIRATE-bootstrap.tar.gz
 
 echo Downloading and extracting Zcash parameters
-mkdir -p /home/$USER/.zcash-params/ && chmod 644 /home/$USER/.zcash-params/
+mkdir -p /home/$USER/.zcash-params/ && chmod 755 /home/$USER/.zcash-params/
 wget -N https://z.cash/downloads/sprout-proving.key -P /home/$USER/.zcash-params
 wget -N https://z.cash/downloads/sprout-verifying.key -P /home/$USER/.zcash-params
 wget -N https://z.cash/downloads/sapling-spend.params -P /home/$USER/.zcash-params
@@ -32,5 +32,5 @@ wget -N https://github.com/PirateNetwork/PirateOcean/releases/download/release_1
 unzip pirate113fbc7_linux.zip
 
 echo Pirateocean Wallet
-chmod 644 pirate-qt-linux
+chmod +x pirate-qt-linux
 ./pirate-qt-linux
