8ac29f6c295f5dcc418ab2e8fc4493a9d99815bd
Kaks <65034388+kaks25@users.noreply.github.com>
2020-11-26 02:05:16 +0100

    Update BS+PirateOcean.bat

--- Bootstrap-Installation-For-Windows/BS+PirateOcean.bat
+++ Bootstrap-Installation-For-Windows/BS+PirateOcean.bat
@@ -1,7 +1,5 @@
-dre@echo off
-
 echo DOWNLOADING Z-ZIP
-wget64.exe -N https://www.7-zip.org/a/7z1900-x64.exe
+IF Windows version !== Windows10 (wget64.exe -N https://www.7-zip.org/a/7z1900-x64.exe)
 
 echo INSTALLING 7-ZIP
 7z1900-x64.exe /S
@@ -18,10 +16,10 @@ echo EXTRACTING BOOTSTRAP
 "C:\Program Files\7-Zip\7z" x "%appdata%\komodo\PIRATE\PIRATE-bootstrap.tar" -o"%appdata%\komodo\PIRATE\"
 
 echo DOWNLOADING PIRATEOCEAN
-wget64.exe -N https://github.com/PirateNetwork/PirateOcean/releases/download/release_4e1aa04/pirate4e1aa04_win.zip
+wget64.exe -N https://github.com/PirateNetwork/PirateOcean/releases/download/release_df8e111/piratedf8e111_win.zip
 
 echo EXTRACTING PIRATEOCEAN
-"C:\Program Files\7-Zip\7z" e pirate4e1aa04_win.zip -o"%~dp0" *.exe -r -y > nul
+"C:\Program Files\7-Zip\7z" e piratedf8e111_win.zip -o"%~dp0" *.exe -r -y > nul
 
 echo LAUNCING PIRATEOCEAN
 start pirate-qt-win.exe
