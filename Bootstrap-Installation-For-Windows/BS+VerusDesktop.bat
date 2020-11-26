32267a4084e76a94af1ac403a9b3d1b46ce47ec5
Kaks <65034388+kaks25@users.noreply.github.com>
2020-11-26 02:05:37 +0100

    Update BS+VerusDesktop.bat

--- Bootstrap-Installation-For-Windows/BS+VerusDesktop.bat
+++ Bootstrap-Installation-For-Windows/BS+VerusDesktop.bat
@@ -13,16 +13,20 @@ rd /s /q "%appdata%\komodo\PIRATE\blocks"
 echo DOWNLOADING BOOTSTRAP
 wget64.exe -N https://eu.bootstrap.dexstats.info/PIRATE-bootstrap.tar.gz
 
+echo EXTRACTING BOOTSTRAP
+"C:\Program Files\7-Zip\7z" e "PIRATE-bootstrap.tar.gz" -o"%appdata%\komodo\PIRATE\"
+"C:\Program Files\7-Zip\7z" x "%appdata%\komodo\PIRATE\PIRATE-bootstrap.tar" -o"%appdata%\komodo\PIRATE\"
+
 echo DOWNLOADING Verus Desktop
-wget64.exe -N https://github.com/VerusCoin/Verus-Desktop/releases/download/v0.7.2-2/Verus-Desktop-Windows-v0.7.2-2.zip
+wget64.exe -N https://github.com/VerusCoin/Verus-Desktop/releases/download/v0.7.2-5/Verus-Desktop-Windows-v0.7.2-5.zip
 
 echo EXTRACTING Verus Desktop
-"C:\Program Files\7-Zip\7z" e Verus-Desktop-Windows-v0.7.2-2.zip -o"%~dp0" *.exe -r -y > nul
+"C:\Program Files\7-Zip\7z" e Verus-Desktop-Windows-v0.7.2-5.zip -o"%~dp0" *.exe -r -y > nul
 
 echo Launching Verus Desktop Wallet
-start Verus-Desktop-Windows-v0.7.2-2.exe
+start Verus-Desktop-Windows-v0.7.2-5.zip
 
-del Verus-Desktop-Windows-v0.7.2-2.zip
+del Verus-Desktop-Windows-v0.7.2-5.zip
 del 7z1900-x64.exe
 del PIRATE-bootstrap.tar.gz
 del %appdata%\komodo\PIRATE\PIRATE-bootstrap.tar
