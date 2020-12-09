05374a4be1a1b47c239a526eaaf37d21204a5280
Kaks <65034388+kaks25@users.noreply.github.com>
2020-12-09 02:59:15 +0100

    Update VerusDesktop.bat

--- Easy-Wallet-Installation-For-Windows/VerusDesktop.bat
+++ Easy-Wallet-Installation-For-Windows/VerusDesktop.bat
@@ -14,11 +14,11 @@ echo EXTRACTING BOOTSTRAP
 "C:\Program Files\7-Zip\7z" x "%appdata%\komodo\PIRATE\PIRATE-bootstrap.tar" -o"%appdata%\komodo\PIRATE\"
 
 echo Downloading Zcash parameters
-    wget64.exe -N https://z.cash/downloads/sprout-proving.key
-    wget64.exe -N https://z.cash/downloads/sprout-verifying.key
-    wget64.exe -N https://z.cash/downloads/sapling-spend.params
-    wget64.exe -N https://z.cash/downloads/sapling-output.params
-    wget64.exe -N https://z.cash/downloads/sprout-groth16.params
+    wget64.exe -N https://download.z.cash/downloads//sprout-proving.key
+    wget64.exe -N https://download.z.cash/downloads//sprout-verifying.key
+    wget64.exe -N https://download.z.cash/downloads//sapling-spend.params
+    wget64.exe -N https://download.z.cash/downloads//sapling-output.params
+    wget64.exe -N https://download.z.cash/downloads//sprout-groth16.params
 
 echo Moving files to %appdata%\ZcashParams
 xcopy "sprout-proving.key" "%appdata%\ZcashParams\"
@@ -29,14 +29,14 @@ move "sprout-groth16.params" "%appdata%\ZcashParams\"
 del sprout-proving.key
 
 echo DOWNLOADING Verus Desktop
-wget64.exe -N https://github.com/VerusCoin/Verus-Desktop/releases/download/v0.7.2-5/Verus-Desktop-Windows-v0.7.2-5.zip
+wget64.exe -N https://github.com/VerusCoin/Verus-Desktop/releases/download/v0.7.2-6/Verus-Desktop-Windows-v0.7.2-6.zip
 
 echo EXTRACTING Verus Desktop
-"C:\Program Files\7-Zip\7z" e Verus-Desktop-Windows-v0.7.2-5.zip -o"%~dp0" *.exe -r -y > nul
+"C:\Program Files\7-Zip\7z" e Verus-Desktop-Windows-v0.7.2-6.zip -o"%~dp0" *.exe -r -y > nul
 
 echo Launching Verus Desktop Wallet
-start Verus-Desktop-v0.7.2-5.exe
+start Verus-Desktop-v0.7.2-6.exe
 
-del Verus-Desktop-Windows-v0.7.2-5.zip
+del Verus-Desktop-Windows-v0.7.2-6.zip
 del 7z1900-x64.exe
 del PIRATE-bootstrap.tar.gz
