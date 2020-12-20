e168f2faebbe054aebdef9ba6c25725aacc6d0eb
Kaks <65034388+kaks25@users.noreply.github.com>
2020-12-20 07:17:22 +0100

    Pirateocean.sh

--- Easy-Wallet-Installation-For-Linux/Pirateocean.sh
+++ Easy-Wallet-Installation-For-Linux/Pirateocean.sh
@@ -11,10 +11,9 @@
 
 echo Creating /.komodo/PIRATE Folder
 mkdir -p /home/$USER/.komodo/PIRATE && chmod 755 /home/$USER/.komodo/PIRATE
+
 echo Installing DOWNLOAD BOOTSRAPS
-wget -N --no-check-certificate --progress=dot:giga --continue --retry-connrefused --waitretry=3 --timeout=30 https://eu.bootstrap.dexstats.info/PIRATE-bootstrap.tar.gz -P /home/$USER/.komodo/PIRATE
-tar -xzvf /home/$USER/.komodo/PIRATE/PIRATE-bootstrap.tar.gz -C /home/$USER/.komodo/PIRATE
-rm ~/.komodo/PIRATE/PIRATE-bootstrap.tar.gz
+wget -N –no-check-certificate –progress=dot:giga –continue –retry-connrefused –waitretry=3 –timeout=30 http://bootstrap.dexstats.info/ARRR-bootstrap.tar.gz -P /home/$USER/.komodo/PIRATE;tar -xzvf /home/$USER/.komodo/PIRATE/ARRR-bootstrap.tar.gz -C /home/$USER/.komodo/PIRATE
 
 echo Downloading and extracting Zcash parameters
 mkdir -p /home/$USER/.zcash-params/ && chmod 755 /home/$USER/.zcash-params/
@@ -25,9 +24,9 @@ wget -N https://download.z.cash/downloads//sapling-output.params -P /home/$USER/
 wget -N https://download.z.cash/downloads//sprout-groth16.params -P /home/$USER/.zcash-params
 
 echo Downloading PirateOcean
-wget -N https://github.com/PirateNetwork/PirateOcean/releases/download/release_df8e111/piratedf8e111_linux.zip
+wget -N https://github.com/PirateNetwork/pirate/releases/download/v3.1.0/pirate-qt-windows-v3.1.0.zip
 
-unzip piratedf8e111_linux.zip
+unzip pirate-qt-windows-v3.1.0.zip
 
 echo Pirateocean Wallet
 chmod +x pirate-qt-linux
