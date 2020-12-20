0cea80d7da08d9a4fc7e17a6dc8b5c32cfd5d03f
Kaks <65034388+kaks25@users.noreply.github.com>
2020-12-20 07:30:07 +0100

    Sevenseas.bat

--- Easy-Wallet-Installation-For-Windows/Sevenseas.bat
+++ Easy-Wallet-Installation-For-Windows/Sevenseas.bat
@@ -1,19 +1,23 @@
+  
+echo Creating Zcash params folder
+mkdir %appdata%\ZcashParams & cd %appdata%\ZcashParams
+
+echo Downloading Zcash parameters
+curl -L -O https://download.z.cash/downloads//sapling-output.params 
+curl -L -O https://download.z.cash/downloads//sprout-groth16.params 
+curl -L -O https://download.z.cash/downloads//sprout-proving.key 
+curl -L -O https://download.z.cash/downloads//sprout-verifying.key 
+curl -L -O https://download.z.cash/downloads//sapling-spend.params
+
 echo CREATING KOMODO AND PIRATE FOLDERS
 mkdir %appdata%\komodo\PIRATE
+cd %appdata%\komodo\PIRATE
 
 echo DOWNLOADING BOOTSTRAP
 curl -L -O http://bootstrap.dexstats.info/ARRR-bootstrap.tar.gz
 
 echo EXTRACTING BOOTSTRAP
 tar -zxf ARRR-bootstrap.tar.gz --directory %appdata%\komodo\PIRATE
-
-echo Downloading Zcash parameters
-
-curl -L -O https://download.z.cash/downloads//sapling-output.params 
-curl -L -O https://download.z.cash/downloads//sprout-groth16.params 
-curl -L -O https://download.z.cash/downloads//sprout-proving.key 
-curl -L -O https://download.z.cash/downloads//sprout-verifying.key 
-curl -L -O https://download.z.cash/downloads//sapling-spend.params
 
 cd %USERPROFILE%\Desktop
 
